d7e1c7faadd2cefe051bd36ab3935f4920f727c1
abagul1 <akhilbagul80@gmail.com>
2020-04-17 10:28:58 -0400

    Initial commit

---
+++ src/cs3500/animator/controller/AnimationController.java
@@ -0,0 +1,122 @@
+package cs3500.animator.controller;
+
+import javax.swing.Timer;
+import cs3500.IAnimation;
+import cs3500.IController;
+import cs3500.IView;
+
+/**
+ * Class that acts as the controller in the MVC format. Interfaces the model with the view.
+ */
+public class AnimationController implements IController {
+  IView view;
+  IAnimation a;
+  Timer t;
+  int speed;
+  int currentTick;
+  boolean paused;
+  boolean loop;
+  int maxTick;
+
+  /**
+   * Constructor for the animation controller.
+   * @param v view to show
+   */
+  public AnimationController(IView v) {
+    this.view = v;
+    this.paused = true;
+    this.currentTick = 0;
+    this.loop = false;
+    this.speed = 1;
+  }
+
+  @Override
+  public void playAnimation(IAnimation a, String viewType, int tempo) {
+    this.a = a;
+    this.speed = tempo;
+    this.maxTick = a.getMaxTick();
+    if (viewType.equals("edit")) {
+      view.addClickListener(this);
+    }
+    view.makeVisible();
+    if (viewType.equals("visual") || viewType.equals("edit")) {
+      t = new Timer(1000 / speed, e -> execute());
+      t.start();
+      t.setRepeats(true);
+    }
+    else {
+      execute();
+    }
+  }
+
+  /**
+   * Executes the view.
+   */
+  private void execute() {
+    if (!paused) {
+      if ((currentTick == maxTick + 1) && loop) {
+        currentTick = 0;
+        a.resetAnimation();
+        maxTick = a.getMaxTick();
+      }
+      view.execute();
+      a.executeTick(currentTick);
+      currentTick++;
+    }
+  }
+
+  @Override
+  public void executeTick(int tick) {
+    this.currentTick = tick;
+    a.executeTick(tick);
+    view.execute();
+  }
+
+  @Override
+  public void changeSpeed(String type) {
+    if (type.equals("+")) {
+      t.setDelay(1000 / speed++);
+    }
+    else {
+      if (speed > 1) {
+        t.setDelay(1000 / speed--);
+      }
+    }
+  }
+
+  @Override
+  public boolean getPaused() {
+    return paused;
+  }
+
+  @Override
+  public void setPaused() {
+    paused = !paused;
+  }
+
+  @Override
+  public boolean getLoop() {
+    return loop;
+  }
+
+  @Override
+  public void setLoop() {
+    loop = !loop;
+  }
+
+  @Override
+  public int getSpeed() {
+    return this.speed;
+  }
+
+  @Override
+  public void setMaxTick(int maxTick) {
+    this.maxTick = maxTick;
+  }
+
+  @Override
+  public void restart() {
+    this.currentTick = 0;
+    this.maxTick = a.getMaxTick();
+  }
+}
---
+++ src/cs3500/elements/Rectangle.java
@@ -0,0 +1,96 @@
+package cs3500.elements;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.util.Objects;
+
+/**
+ * Element to represent a rectangle.
+ */
+public class Rectangle extends AbstractElement {
+  private double height;
+  private double width;
+
+  /**
+   * Constructor for a rectangle.
+   * @param id element id
+   * @param c color
+   * @param p posn
+   * @param height height
+   * @param width width
+   */
+  public Rectangle(String id, Color c, Posn p, double height, double width, double angle) {
+    super(c, p, id, angle);
+    this.height = height;
+    this.width = width;
+  }
+
+  /**
+   * Gets the height of an rectangle.
+   * @return height
+   */
+  private double getHeight() {
+    return this.height;
+  }
+
+  /**
+   * Gets the width of an rectangle.
+   * @return width
+   */
+  private double getWidth() {
+    return this.width;
+  }
+
+  /**
+   * Returns an array of doubles of length 2, in the following order: Height, Width.
+   * @return  the array of dimensions.
+   */
+  @Override
+  public double[] getDimensions() {
+    return new double[]{getHeight(), getWidth()};
+  }
+
+  @Override
+  public void setWidth(double w) {
+    this.width = w;
+  }
+
+  @Override
+  public void setHeight(double h) {
+    this.height = h;
+  }
+
+  @Override
+  public String getType() {
+    return "rectangle";
+  }
+
+  @Override
+  public boolean equals(Object that) {
+    if (this == that) {
+      return true;
+    }
+    if (!(that instanceof Rectangle)) {
+      return false;
+    }
+    return (this.getID().equals(((Rectangle) that).getID()));
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getID());
+  }
+
+  @Override
+  public void paint(Graphics2D g2d) {
+    super.paint(g2d);
+    Rectangle2D rect = new Rectangle2D.Double(getPosn().getX(), getPosn().getY(), width, height);
+    AffineTransform a = new AffineTransform();
+    a.rotate(Math.toRadians(angle % 360), rect.getX() + rect.getWidth() / 2,
+            rect.getY() + rect.getHeight() / 2 );
+    Shape rotatedRect = a.createTransformedShape(rect);
+    g2d.fill(rotatedRect);
+    g2d.draw(rotatedRect);
+  }
+}
